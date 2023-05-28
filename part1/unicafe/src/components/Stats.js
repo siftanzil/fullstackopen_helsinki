a55aa7d29e2c966eadb2357dade4017b79206620
siftanzil <siftanzil@gmail.com>
2023-05-28 16:53:08 +0600

    part1 1.7 completed

--- part1/unicafe/src/components/Stats.js
+++ part1/unicafe/src/components/Stats.js
@@ -2,13 +2,29 @@ import React from "react";
 
 const Stats = ({ good, neutral, bad }) => {
    //    console.log("g - " + good + ", n - " + neutral + ", b - " + bad);
+   let all = good + neutral + bad;
+   //    console.log(all);
+   let average = (good * 1 + neutral * 0 + bad * -1) / all;
+   //    console.log(average);
+   let positive = (good / all) * 100;
+   //    console.log(positive);
 
    return (
       <div>
          <h1>statistics</h1>
-         <p>good {good}</p>
-         <p>neutral {neutral}</p>
-         <p>bad {bad}</p>
+         <p>
+            good {good}
+            <br />
+            neutral {neutral}
+            <br />
+            bad {bad}
+            <br />
+            all {all}
+            <br />
+            average {average ? average : 0}
+            <br />
+            positive {positive ? positive : 0} %
+         </p>
       </div>
    );
 };
