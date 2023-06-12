648037dca15e08833c23935e781910b3c57eeef4
siftanzil <siftanzil@gmail.com>
2023-06-12 18:34:38 +0600

    part2 2.18

---
+++ part2/countrydata/src/App.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+// import services from "./services/country";
+import data from "./services/data";
+
+const App = () => {
+   const [countries, setCountries] = useState([]);
+   const [searched, setSearched] = useState([]);
+   const [search, setSearch] = useState("");
+
+   useEffect(() => {
+      // services.getAll().then((response) => {
+      //    console.log(response.data);
+      //    setCountries(response.data);
+      // });
+      setCountries(data);
+      console.log(countries[0]);
+   }, []);
+
+   const handleChange = (search) => {
+      console.log(search);
+      setSearch(search);
+      setSearched(
+         countries.filter((country) =>
+            country.name.common.toLowerCase().includes(search),
+         ),
+      );
+   };
+
+   return (
+      <div>
+         <p>find countries</p>
+         <input value={search} onChange={(e) => handleChange(e.target.value)} />
+         {searched.length > 10 && (
+            <p>Too many matches, specify another filter</p>
+         )}
+         {searched.length !== 1 &&
+            searched.length <= 10 &&
+            searched.map((country, key) => {
+               return (
+                  <p key={key}>
+                     {country.name.common + " - " + country.capital}
+                  </p>
+               );
+            })}
+         {searched.length === 1 &&
+            searched.map((country, key) => {
+               return (
+                  <div key={key}>
+                     <h1>{country.name.common}</h1>
+                     <br />
+                     <h3>languages</h3>
+                     <ul>
+                        {Object.keys(country.languages).map((key, index) => {
+                           return <li key={index}>{country.languages[key]}</li>;
+                        })}
+                     </ul>
+                     <img src={country.flags.png} alt="Logo" />
+                  </div>
+               );
+            })}
+      </div>
+   );
+};
+
+export default App;
---
+++ part2/countrydata/src/services/country.js
@@ -0,0 +1,15 @@
+// import axios from "axios";
+// const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api";
+
+// const getAll = () => {
+//    return axios.get(`${baseUrl}/all`);
+// };
+
+// const search = (query) => {
+//    return axios.get(`${baseUrl}/name/${query}`);
+// };
+
+// export default {
+//    getAll,
+//    search,
+// };
